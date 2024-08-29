c4cd7411de6910384f9a5dfa39cd7e0629239640
ni-shat <nishat.tabassum301@gmail.com>
2024-08-29 17:25:29 +0600

    added icon

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,24 @@
 import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 import { ModalProvider } from "./context/ModalContext";
+
 const poppins = Poppins({
   subsets: ["latin"],
   weight: "400"
 });
 
 export const metadata = {
-  title: "One Path project",
+  title: "One Path Project",
   description: "A user-friendly donation platform",
+  icons: {
+    icon: '/images/logoIcon.jpg', 
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={poppins.className}>
-
         <ModalProvider>
           {children}
         </ModalProvider>
